90c9aa60dcff640f964c202f8164da96ca155d73
ValerioDiSano <valerio.disano@fcagroup.com>
2020-04-01 20:57:35 +0200

    feat: changeState implementation for Twiddle class.
    changeState method is used to make transitions in
    the twiddle algorithm.

--- src/twiddle.cpp
+++ src/twiddle.cpp
@@ -5,3 +5,9 @@ void TwiddleState::setContext(Twiddle* context)
 {
     this->context_ = std::make_shared(context);
 }
+
+void Twiddle::changeState(TwiddleState* state)
+{
+    this->current_state = std::make_unique<Twiddle>(state);
+    this->current_state.setContext(this);
+}
