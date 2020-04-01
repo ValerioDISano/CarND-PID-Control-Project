eac9be4ad573a100c785e9d3910ea9636e48e398
ValerioDiSano <valerio.disano@fcagroup.com>
2020-04-01 20:37:29 +0200

    Feat: State pattern to model Twiddle algorithm steps.

--- src/twiddle.hpp
+++ src/twiddle.hpp
@@ -1,13 +1,53 @@
 #ifndef TWIDDLE_HPP
 #define TWIDDLE_HPP
 
+#include <memory>
+#include <vector>
+
+class TwiddleState
+{
+  protected:
+      std::shared_ptr<Twiddle> context_;
+  public:
+      std::string state;
+      virtual void run() = 0;
+
+      void setContext(Twiddle* context);
+};
+
+class TwiddleStart : public TwiddleState
+{
+  public:
+    void run() override;
+  
+};
+
+class TwiddleIncrease : public TwiddleState
+{
+  public:
+    void run() override;
+};
+
+class TwiddleDecrease : public TwiddleState
+{
+  public:
+    void run() override;
+};
+
+
 class Twiddle
 {
   public:
     Twiddle()
     ~Twiddle()
+
+    void changeState(TwiddleState* state);
   private:
     void Run();
-}
+    std::vector<double> gains;
+    std::vector<double> gains_delta;
+    std::vector<double> best_solution;
+    std::unique_ptr<TwiddleState> current_state;
+};
 
 #endif // TWIDDLE_HPP
