682a875d82e1205403285cf5a1d6977abb6d5547
ValerioDiSano <valerio.disano@fcagroup.com>
2020-04-01 21:53:41 +0200

    fix: substitute make_unique fcn with unique_ptr ctor.
    unique_ptr ctor is used instead of make_unique std fcn to
    be compliant with the C++11 standard.

--- src/twiddle.cpp
+++ src/twiddle.cpp
@@ -2,25 +2,35 @@
 
 void TwiddleState::setContext(Twiddle* context)
 {
-    this->context_ = std::make_shared(context);
+    this->context_ = std::shared_ptr<Twiddle>(context);
 }
 
 void Twiddle::changeState(TwiddleState* state)
 {
-    this->current_state = std::make_unique<Twiddle>(state);
-    this->current_state.setContext(this);
+    current_state_ = std::unique_ptr<TwiddleState>(state);
+    current_state_->setContext(this);
 }
 
 void TwiddleStart::run()
 {
-    if std::accumulate(context_->gains_delta_.begin(),
-            context_->gains_delta_.end(), 0.0) < context_->tolerance_
+    if (std::accumulate(context_->gains_delta_.begin(),
+            context_->gains_delta_.end(), 0.0) < context_->tolerance_)
     {
         context_->best_solution_ = context_->gains_;
         return;
     }
 
-    context_->gains_[context_->index] += context_->gains_delta_[context_->index];
+    context_->gains_[context_->index_] += context_->gains_delta_[context_->index_];
 
-    context_->changeState(new TwiddleIncrease())
+    context_->changeState(new TwiddleIncrease());
+}
+
+void TwiddleIncrease::run()
+{
+    ;
+}
+
+void TwiddleDecrease::run()
+{
+    ;
 }
