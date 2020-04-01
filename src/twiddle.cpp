8d71372e36a17eda5901a8d9b96461a8335f4df2
ValerioDiSano <valerio.disano@fcagroup.com>
2020-04-01 21:52:24 +0200

    style: missing underscore in variable names.

--- src/twiddle.cpp
+++ src/twiddle.cpp
@@ -1,6 +1,5 @@
 #include "twiddle.hpp"
 
-
 void TwiddleState::setContext(Twiddle* context)
 {
     this->context_ = std::make_shared(context);
@@ -14,14 +13,14 @@ void Twiddle::changeState(TwiddleState* state)
 
 void TwiddleStart::run()
 {
-    if std::accumulate(context_->gains_delta.begin(),
-            context_->gains_delta.end(), 0.0) < context_->tolerance
+    if std::accumulate(context_->gains_delta_.begin(),
+            context_->gains_delta_.end(), 0.0) < context_->tolerance_
     {
-        context_->best_solution = gains;
+        context_->best_solution_ = context_->gains_;
         return;
     }
 
-    gains[index] += gains_delta[index];
+    context_->gains_[context_->index] += context_->gains_delta_[context_->index];
 
     context_->changeState(new TwiddleIncrease())
 }
