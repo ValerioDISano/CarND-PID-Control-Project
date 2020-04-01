de10c761f94bbfe26e5109d0e0a3c06801b4cc14
ValerioDiSano <valerio.disano@fcagroup.com>
2020-04-01 21:07:49 +0200

    feat: virtual method implementation for Start Twiddle state.
    Start state tries to increment PID gains.

--- src/twiddle.cpp
+++ src/twiddle.cpp
@@ -11,3 +11,17 @@ void Twiddle::changeState(TwiddleState* state)
     this->current_state = std::make_unique<Twiddle>(state);
     this->current_state.setContext(this);
 }
+
+void TwiddleStart::run()
+{
+    if std::accumulate(context_->gains_delta.begin(),
+            context_->gains_delta.end(), 0.0) < context_->tolerance
+    {
+        context_->best_solution = gains;
+        return;
+    }
+
+    gains[index] += gains_delta[index];
+
+    context_->changeState(new TwiddleIncrease())
+}
